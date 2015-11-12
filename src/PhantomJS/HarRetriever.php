3ffa7f20b24f07ca78314864f61293616d4fe1ef
Langner <nils.langner@hbv83033342.bauer-de.bauermedia.group>
2015-11-12 17:31:04 +0100

    phar archive will now store netsniff.js in tmp dir

--- src/PhantomJS/HarRetriever.php
+++ src/PhantomJS/HarRetriever.php
@@ -8,18 +8,27 @@ class HarRetriever
 {
     private $phantomJSExec = "phantomjs";
     private $netsniffFile = "netsniff.js";
+    private $netSniffTempFile;
 
     public function __construct($phantomJSExec = null)
     {
         if (!is_null($phantomJSExec)) {
             $this->phantomJSExec = $phantomJSExec;
         }
+
+        $this->netSniffTempFile = \tempnam("missing", 'netsniff_');
+        copy(__DIR__ . "/" . $this->netsniffFile, $this->netSniffTempFile);
     }
 
     public function getHarFile(UriInterface $uri)
     {
-        $command = $this->phantomJSExec . " " . __DIR__ . "/" . $this->netsniffFile . " " . (string)$uri;
+        $command = $this->phantomJSExec . " " . $this->netSniffTempFile . " " . (string)$uri;
         exec($command, $output);
         return new HarArchive(json_decode(implode($output, "\n")));
+    }
+
+    public function __destruct()
+    {
+        unlink($this->netSniffTempFile);
     }
 }

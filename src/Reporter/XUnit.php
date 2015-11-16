5fc25556f677f2f4a90e3072aca83bd0376862d8
Langner <nils.langner@hbv83033342.bauer-de.bauermedia.group>
2015-11-16 16:40:56 +0100

    updated to XUnitReporter 1.0.2

--- src/Reporter/XUnit.php
+++ src/Reporter/XUnit.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace whm\MissingRequest\Reporter;
+
 use phmLabs\XUnitReport\Elements\Failure;
 use phmLabs\XUnitReport\Elements\TestCase;
 use phmLabs\XUnitReport\XUnitReport;
@@ -9,6 +10,8 @@ class XUnit implements Reporter
 {
     private $report;
 
+    private $tests;
+
     public function __construct()
     {
         $this->report = new XUnitReport("MissingRequest");
@@ -16,16 +19,30 @@ class XUnit implements Reporter
 
     public function addTestcase($url, $mandatoryUrl, $isFailure)
     {
-        $testCase = new TestCase("MissingRequest", $url . " requests " . $mandatoryUrl, 0);
         if ($isFailure) {
-            $testCase->setFailure(new Failure("Missing request", "Request was not found (" . $mandatoryUrl . ")"));
+            $this->tests[$url][] = $mandatoryUrl;
+        } else {
+            $this->tests[$url][] = false;
         }
 
-        $this->report->addTestCase($testCase);
+
     }
 
     public function getReport()
     {
+
+        var_dump($this->tests);
+
+        foreach ($this->tests as $url => $missingUrls) {
+            $testCase = new TestCase("MissingRequest", $url, 0);
+            foreach ($missingUrls as $missingUrl) {
+                if ($missingUrl !== false) {
+                    $testCase->addFailure(new Failure("Missing request", "Request was not found (" . $missingUrl . ")"));
+                }
+            }
+            $this->report->addTestCase($testCase);
+        }
+
         return $this->report->toXml();
     }
 }

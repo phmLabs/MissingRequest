4bfd615c0ab21fc3b725ec66d75c4281965233e1
Langner <nils.langner@hbv83033342.bauer-de.bauermedia.group>
2015-11-20 16:59:25 +0100

    phantomjs set http header to mozilla

--- src/PhantomJS/netsniff.js
+++ src/PhantomJS/netsniff.js
@@ -101,6 +101,8 @@ if (system.args.length === 1) {
     phantom.exit(1);
 } else {
 
+    page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
+
     page.address = system.args[1];
     page.resources = [];
 
